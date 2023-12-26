6bb1d34905762cbfb8c6409e2fc34edc0e56aebb
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-12-26 13:50:22 -0300

    added button to add photo, success toast and info window image display

--- scripts.js
+++ scripts.js
@@ -27,16 +27,20 @@ function addPin() {
     return;
   }
 
-  const isDuplicate = markers.some((marker) => marker.getTitle() === place.name);
+  const country = getAddressComponent(place, "country");
+  const city = getAddressComponent(place, "locality");
+  const listItemText = country && city ? `${country} - ${city}` : place.name;
+
+  const formattedTitle = listItemText.toLowerCase();
+  const isDuplicate = markers.some((marker) => {
+    const markerTitleLowerCase = marker.getTitle().toLowerCase();
+    return markerTitleLowerCase === formattedTitle;
+  });
 
   if (isDuplicate) {
     showErrorToast("Duplicated place");
     return;
   }
-
-  const country = getAddressComponent(place, "country");
-  const city = getAddressComponent(place, "locality");
-  const listItemText = country && city ? `${country} - ${city}` : place.name;
 
   const marker = new google.maps.Marker({
     position: place.geometry.location,
@@ -46,15 +50,8 @@ function addPin() {
     label: getCountryCode(place),
   });
 
-  //pin tooltip
-  const buttonHtml =
-    "<button class='text-white cursor-pointer group relative flex gap-1.5 justify-center items-center px-8 py-4 bg-black bg-opacity-80 text-[#f1f1f1] rounded-3xl hover:bg-opacity-70 transition font-semibold shadow-md' onclick=\"console.log('Hello World')\"><i class='fas fa-camera'></i> Add Photo</button>";
-  const contentString = `<div>${buttonHtml}</div>`;
-
-  const infoWindow = createInfoWindow(contentString);
-  saveInfoWindowToLocalStorage(marker.getTitle(), contentString);
   marker.addListener("click", () => {
-    infoWindow.open(map, marker);
+    showInfoWindow(marker, getPhotosForMarker(marker.getTitle()));
   });
 
   map.setCenter(marker.getPosition());
@@ -63,19 +60,37 @@ function addPin() {
   // Add pin to the list
   const pinList = document.querySelector(".pinList");
   const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "flex gap-3 items-center justify-center";
+
   li.className =
     "px-2 text-black mb-2 rounded-md flex justify-between items-center py-1 hover:bg-gray-200 cursor-pointer";
   li.textContent = listItemText;
   li.classList.add("fadeIn");
 
+  const addPhotoButton = document.createElement("button");
+  addPhotoButton.className = "text-gray-400 hover:text-black transition-all duration-300 w-6 h-6";
+  addPhotoButton.innerHTML = "<i class='fas fa-camera'></i>";
+
+  const inputPhoto = document.createElement("input");
+  inputPhoto.type = "file";
+  inputPhoto.style.display = "none";
+  inputPhoto.addEventListener("change", (event) => handleFileUpload(event, marker));
+  addPhotoButton.appendChild(inputPhoto);
+
+  addPhotoButton.addEventListener("click", () => {
+    inputPhoto.click();
+  });
+
   const deleteButton = document.createElement("button");
-  deleteButton.textContent = "X"; //trash icon maybe?
-  deleteButton.className =
-    "px-2 py-1 text-white rounded-full hover:bg-red-500 deletePin-btn w-6 h-6 flex justify-center items-center";
+  deleteButton.textContent = "X";
+  deleteButton.className = "text-gray-400 hover:text-black transition-all duration-300 font-bold w-6 h-6";
   deleteButton.addEventListener("click", () => {
     removePin(marker, li);
   });
-  li.appendChild(deleteButton);
+  div.appendChild(addPhotoButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
 
   pinList.appendChild(li);
 
@@ -147,7 +162,7 @@ function removePin(marker, item) {
   }
   removePolyline(markerIndex);
 
-  removeInfoWindowFromLocalStorage(marker.getTitle());
+  removePhotosForMarker(marker.getTitle());
 
   item.remove();
   saveMarkersToLocalStorage();
@@ -190,19 +205,32 @@ function showErrorToast(message) {
     close: true,
     gravity: "top",
     position: "center",
-    backgroundColor: "linear-gradient(to right, #ff9999, #ffcc99)",
+    background: "linear-gradient(to right, #ff9999, #ffcc99)",
+  }).showToast();
+}
+
+function showSuccessToast(message) {
+  Toastify({
+    text: message,
+    duration: 3000,
+    close: true,
+    gravity: "top",
+    position: "center",
+    background: "linear-gradient(to right, #99ff99, #ccff99)",
   }).showToast();
 }
 
 function saveMarkersToLocalStorage() {
   const markersData = markers.map((marker) => {
+    const markerTitle = marker.getTitle();
     return {
-      title: marker.getTitle(),
+      title: markerTitle,
       position: {
         lat: marker.getPosition().lat(),
         lng: marker.getPosition().lng(),
       },
       label: marker.getLabel(),
+      infoWindowContent: getPhotosForMarker(markerTitle),
     };
   });
 
@@ -224,11 +252,24 @@ function loadMarkersFromLocalStorage() {
         label: markerData.label,
       });
 
+      marker.addListener("click", () => {
+        showInfoWindow(marker, getPhotosForMarker(marker.getTitle()));
+      });
+
       markers.push(marker);
+
+      const markerPhotos = loadPhotosForMarker(markerData.title);
+
+      showInfoWindow(marker, markerPhotos);
     });
 
     drawPolylines();
   }
+}
+function loadPhotosForMarker(markerTitle) {
+  const photosData = JSON.parse(localStorage.getItem("photos")) || [];
+
+  return photosData.filter((photoData) => photoData.markerTitle === markerTitle);
 }
 
 function loadPinList() {
@@ -240,22 +281,46 @@ function loadPinList() {
 
     parsedMarkers.forEach((markerData) => {
       const li = document.createElement("li");
+      const div = document.createElement("div");
+      div.className = "flex gap-3 items-center justify-center";
+
       li.className =
         "px-2 text-black mb-2 rounded-md flex justify-between items-center py-1 hover:bg-gray-200 cursor-pointer";
       li.textContent = markerData.title;
       li.classList.add("fadeIn");
 
+      const addPhotoButton = document.createElement("button");
+      addPhotoButton.className = "text-gray-400 hover:text-black transition-all duration-300 w-6 h-6";
+      addPhotoButton.innerHTML = "<i class='fas fa-camera'></i>";
+
+      const inputPhoto = document.createElement("input");
+      inputPhoto.type = "file";
+      inputPhoto.style.display = "none";
+
+      addPhotoButton.addEventListener(
+        "click",
+        (function (marker) {
+          return function () {
+            inputPhoto.click();
+          };
+        })(markerData)
+      );
+
+      inputPhoto.addEventListener("change", (event) => handleFileUpload(event, markerData));
+
       const deleteButton = document.createElement("button");
-      deleteButton.textContent = "X"; //trash icon maybe?
+      deleteButton.textContent = "X";
       deleteButton.className =
-        "px-2 py-1 text-white rounded-full hover:bg-red-500 deletePin-btn w-6 h-6 flex justify-center items-center";
+        "px-2 py-1 text-gray-400 hover:text-black transition-all duration-300 rounded-full font-bold w-6 h-6 flex justify-center items-center";
       deleteButton.addEventListener("click", () => {
         const markerToDelete = markers.find((marker) => marker.getTitle() === markerData.title);
         if (markerToDelete) {
           removePin(markerToDelete, li);
         }
       });
-      li.appendChild(deleteButton);
+      div.appendChild(addPhotoButton);
+      div.appendChild(deleteButton);
+      li.appendChild(div);
       pinList.appendChild(li);
 
       makeElementDraggable(li);
@@ -263,36 +328,82 @@ function loadPinList() {
   }
 }
 
-function createInfoWindow(content) {
-  return new google.maps.InfoWindow({
+function handleFileUpload(event, marker) {
+  const fileInput = event.target;
+  const file = fileInput.files[0];
+
+  if (!file || !file.type.startsWith("image/")) {
+    showErrorToast("Invalid file type");
+    return;
+  }
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const photoData = {
+        markerTitle: marker.title,
+        dataURL: e.target.result,
+      };
+
+      savePhotoToLocalStorage(photoData);
+      showSuccessToast("Photo added successfully");
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
+
+function savePhotoToLocalStorage(photoData) {
+  const photosData = JSON.parse(localStorage.getItem("photos")) || [];
+
+  photosData.push(photoData);
+
+  localStorage.setItem("photos", JSON.stringify(photosData));
+}
+
+function showInfoWindow(marker, photos) {
+  const content = generateInfoWindowContent(photos);
+
+  const infoWindow = new google.maps.InfoWindow({
     content: content,
   });
+
+  infoWindow.open(map, marker);
 }
 
-function saveInfoWindowToLocalStorage(markerTitle, contentString) {
-  const infoWindowsData = JSON.parse(localStorage.getItem("infoWindows")) || {};
-  infoWindowsData[markerTitle] = contentString;
-  localStorage.setItem("infoWindows", JSON.stringify(infoWindowsData));
-}
+function generateInfoWindowContent(photos) {
+  if (photos.length === 0) {
+    return "No photos available";
+  }
 
-function loadInfoWindowsFromLocalStorage() {
-  const infoWindowsData = JSON.parse(localStorage.getItem("infoWindows")) || {};
+  const content = document.createElement("div");
+  content.className = "info-window-content";
 
-  Object.entries(infoWindowsData).forEach(([markerTitle, contentString]) => {
-    const marker = markers.find((m) => m.getTitle() === markerTitle);
+  photos.forEach((photo) => {
+    const imgContainer = document.createElement("div");
+    imgContainer.className = "photo-container";
 
-    if (marker) {
-      const infoWindow = createInfoWindow(contentString);
+    const img = document.createElement("img");
+    img.src = photo.dataURL;
+    img.alt = "Marker Photo";
 
-      marker.addListener("click", () => {
-        infoWindow.open(map, marker);
-      });
-    }
+    imgContainer.appendChild(img);
+    content.appendChild(imgContainer);
   });
+
+  return content;
 }
 
-function removeInfoWindowFromLocalStorage(markerTitle) {
-  const infoWindowsData = JSON.parse(localStorage.getItem("infoWindows")) || {};
-  delete infoWindowsData[markerTitle];
-  localStorage.setItem("infoWindows", JSON.stringify(infoWindowsData));
+function getPhotosForMarker(markerTitle) {
+  const photosData = JSON.parse(localStorage.getItem("photos")) || [];
+  return photosData.filter((photoData) => photoData.markerTitle === markerTitle);
+}
+
+function removePhotosForMarker(markerTitle) {
+  const photosData = JSON.parse(localStorage.getItem("photos")) || [];
+
+  const updatedPhotos = photosData.filter((photoData) => photoData.markerTitle !== markerTitle);
+
+  localStorage.setItem("photos", JSON.stringify(updatedPhotos));
 }
